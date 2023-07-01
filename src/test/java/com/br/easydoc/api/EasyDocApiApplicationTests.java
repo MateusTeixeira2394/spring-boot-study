af27295561da2f89b516ecb2b94ab026121f91f5
Mateus Teixeira <mateus.teixeira2394@gmail.com>
2023-07-01 16:23:31 -0300

    Project creation

---
+++ src/test/java/com/br/easydoc/api/EasyDocApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.easydoc.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EasyDocApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
